0b87c941adb4010d881d7d461c28873fa56918a7
diego <diegcapelli@gmail.com>
2021-11-23 04:06:18 -0300

    Se guarda en archivo contactos, ajustes generales

--- src/app.js
+++ src/app.js
@@ -4,27 +4,34 @@ var app = express();
 var cors = require('cors');
 app.use(express.json());
 app.use(cors());
- 
- 
-app.get('/experiencia-laboral', function (req, res) {
 
-  const cvData1 = { empresa: "Empresa X", puesto: "Fullstack Developer",
-                     descripcion: "Webs basadas en PHP ,Java spring MVC,primafaces,webservices en java (JAX-RS,java EE),base de datos Oracle SQL,PL/SQL , despliegue y generación de aplicación para servidores Wildfly y Apache Tomcat", 
-                     fechaInicio: new Date("2014-04-04"), 
-                     fechaFin: new Date("2016-04-04") };
 
-  const cvData2 = { empresa: "Empresa Y", puesto: "Fullstack Developer", 
-                    descripcion: "De todo un poco",
-                    fechaInicio: new Date("2017-04-04"),
-                    fechaFin: new Date("2019-04-04") };
+app.get('/experiencia-laboral', function(req, res) {
 
-  const cvData3 = { empresa: "Empresa Z", puesto: "Fullstack Developer", 
-                    descripcion: "Programar saladamente de todo un Poco",
-                    fechaInicio: new Date("2020-04-04"),
-                    fechaFin: new Date("2021-04-04") };                  
+  
+  const cvData1 = {
+    empresa: "Empresa X", puesto: "Fullstack Developer",
+    descripcion: "Webs basadas en PHP ,Java spring MVC,primafaces,webservices en java (JAX-RS,java EE),base de datos Oracle SQL,PL/SQL , despliegue y generación de aplicación para servidores Wildfly y Apache Tomcat",
+    fechaInicio: new Date("2014-04-04"),
+    fechaFin: new Date("2016-04-04")
+  };
+
+  const cvData2 = {
+    empresa: "Empresa Y", puesto: "Fullstack Developer",
+    descripcion: "De todo un poco",
+    fechaInicio: new Date("2017-04-04"),
+    fechaFin: new Date("2019-04-04")
+  };
+
+  const cvData3 = {
+    empresa: "Empresa Z", puesto: "Fullstack Developer",
+    descripcion: "Programar saladamente de todo un Poco",
+    fechaInicio: new Date("2020-04-04"),
+    fechaFin: new Date("2021-04-04")
+  };
 
   res.writeHead(200, { "Content-Type": "application/json" });
-  var otherArray = [cvData1, cvData2,cvData3];
+  var otherArray = [cvData1, cvData2, cvData3];
   var json = JSON.stringify({
     "experiencia-laboral": otherArray,
   });
@@ -33,20 +40,43 @@ app.get('/experiencia-laboral', function (req, res) {
 });
 
 
-app.post('/enviar-formulario', function (req, res) {
+app.post('/enviar-formulario', function(req, res) {
  
-  if (req.body.nombreContacto == null) {
+  if (req.body.nombreContacto == null || req.body.nombreContacto=='') {
+    
     res.statusCode = 400;
     res.end("Falta el nombre de contacto");
   } else {
+   
     res.cookie('PW_2021-CV_Contacto', req.body.nombreContacto);
-    res.end("datos agregados con exito");
+
+    var fs = require('fs')
+    var writeData = fs.createWriteStream('data.txt', {
+      flags: 'a' // 'a' means appending (old data will be preserved)
+    })
+  let dataToSave = req.body.nombreContacto + ',' + req.body.email + ',' +
+            req.body.lastname + ',' + req.body.phone + ',' +
+            req.body.motive +'\n';
+    writeData.write(dataToSave)
+    writeData.end(); 
+
+    /* 
+        var readStream = fs.createReadStream('data.txt');
+    
+        readStream.on('data', function(chunk) {
+          console.log(chunk.toString());
+    
+        }); 
+    */
+ 
+    var json = JSON.stringify("Datos agregados con exito");
+    res.end(json);
   }
 
 
 });
 
-app.post('/*', function (req, res) {
+app.post('/*', function(req, res) {
 
   res.statusCode = 404;
   res.end("404 - No fue encontrado");
